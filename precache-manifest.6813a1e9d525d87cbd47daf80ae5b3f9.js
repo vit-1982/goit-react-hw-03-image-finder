e299f30aa98f86283a1409681345f94ca003a083
Vitalii Zelenyi <vzntst@gmail.com>
2020-10-20 18:10:30 +0300

    Updates

---
+++ precache-manifest.6813a1e9d525d87cbd47daf80ae5b3f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f729af8d974150e63844229ab17c1b44",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "07f0416213db23b15c50",
+    "url": "/goit-react-hw-03-image-finder/static/css/2.f87e9f44.chunk.css"
+  },
+  {
+    "revision": "af619300b7d79a328996",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.2f92f99e.chunk.css"
+  },
+  {
+    "revision": "07f0416213db23b15c50",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.fef5da32.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.fef5da32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af619300b7d79a328996",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.97e72d3d.chunk.js"
+  },
+  {
+    "revision": "00ed64b4697a879a9e95",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.4e03020b.js"
+  }
+]);
